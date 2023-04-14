b62116d2d41a28ec22d7f74da62312d1f1b0ea45
Takahiro Tsuchiya <d.m_ro.nl.e@icloud.com>
2023-04-14 07:32:21 +0000

    Implement "Get started"

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import "./styles.css";
+// Import everything needed to use the `useQuery` hook
+import { useQuery, gql } from "@apollo/client";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(
+    ({
+      id,
+      name,
+      description,
+      photo,
+    }: {
+      id: string;
+      name: string;
+      description: string;
+      photo: string;
+    }) => (
+      <div key={id}>
+        <h3>{name}</h3>
+        <img
+          width="400"
+          height="250"
+          alt="location-reference"
+          src={`${photo}`}
+        />
+        <br />
+        <b>About this location:</b>
+        <p>{description}</p>
+        <br />
+      </div>
+    )
+  );
+}
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayLocations />
     </div>
   );
 }
